98b9aba7d76c3bbb0d238faa3d38ce9b9e06a781
yahooserg <seltexinc@gmail.com>
2017-06-27 17:45:59 +0300

    add 2 dinamic pages and add query for all cat

--- server.js
+++ server.js
@@ -33,7 +33,7 @@
     next();
   });
 
-  app.get('/catalog/:partId', function (req, res) {
+  app.get('/catalog/part/:partId', function (req, res) {
 
     var query = "SELECT * FROM inventory WHERE id = " + req.params.partId,
       connection = mysql.createConnection(mysqlConnection);
@@ -42,15 +42,58 @@
 
     connection.query(query, function (err, rows, fields) {
       // console.log(rows[0]);
-      res.render('index', {part: rows[0]});
+      res.render('part', {part: rows[0]});
     });
+
+    connection.end();
 
   });
 
-  app.get('/catalog/', function (req, res) {
+  app.get(['/catalog/:search','/catalog/'], function (req, response) {
 
-      res.render('index', {part: {description: 'hkjl', qty: 12, qtyOrdered: 2}});
+    var query = '',
+      connection = mysql.createConnection(mysqlConnection),
+      search = '',
+      i,
+      items = [];
 
+    //prepare sql query
+    if (req.params.search) {
+      search = req.params.search;
+      search = search.split(' ');
+      for (i = 0; i < search.length; i += 1) {
+        if (!search[i]) {
+          search.splice(i,1);
+          i -= 1;
+        }
+      }
+      console.log(search);
+    } else {
+      query = 'SELECT p.ID as ProductID, p.Description AS ProductName, p.Price as ListPrice, p.Numbers AS CategoryName, p.stock as qty, p.ordered as qtyOrdered, p.link as link from inventory as p order by p.Description limit 10';
+    }
+
+    query = connection.query(query);
+
+    query
+      .on('result', function (row, index) {
+          items[items.length] = row;
+      })
+      .on('end', function () {
+        response.render('search', {searchPhrase: search, items: items, length: items.length});
+
+      });
+
+    // response.render('search', {searchPhrase: 'asdf', items : []});
+
+    connection.end();
+
+
+  });
+
+
+  app.get('/*', function (req, res) {
+
+    res.render('notfound', {part: {description: 'Страницы не существует', stock: 0, ordered: 0}});
 
   });
 
