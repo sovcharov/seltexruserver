aa749701866494f1766dab08803abebfeb4a94b8
yahooserg <seltexinc@gmail.com>
2017-06-30 19:56:33 +0300

    add mailer

--- server.js
+++ server.js
@@ -3,16 +3,17 @@
   "use strict";
 
   var express = require('express'),
-    app = express(),
-    mysql = require('mysql'),
-    mysqlConnection = require(__dirname + '/../serverconfig/dbconnectmysqlnode.js'),
-    port = require(__dirname + '/../serverconfig/nodeconfig.js').serverPort,
-    staticSitePath = require(__dirname + '/../serverconfig/nodeconfig.js').staticSitePath,
-    getRidOfEmptyItems = require('./functions/myfunctions').getRidOfEmptyItems,
-    createComplicatedQuery = require('./functions/myfunctions').createComplicatedQuery,
-    checkIfCat = require('./functions/myfunctions').checkIfCat,
-    http,
-    httpServer;
+  app = express(),
+  mysql = require('mysql'),
+  nodemailer = require('nodemailer'),
+  mysqlConnection = require(__dirname + '/../serverconfig/dbconnectmysqlnode.js'),
+  port = require(__dirname + '/../serverconfig/nodeconfig.js').serverPort,
+  staticSitePath = require(__dirname + '/../serverconfig/nodeconfig.js').staticSitePath,
+  getRidOfEmptyItems = require('./functions/myfunctions').getRidOfEmptyItems,
+  createComplicatedQuery = require('./functions/myfunctions').createComplicatedQuery,
+  checkIfCat = require('./functions/myfunctions').checkIfCat,
+  http,
+  httpServer;
 
   http = require('http');
   httpServer = http.createServer(app);
@@ -39,7 +40,7 @@
   app.get('/catalog/part/:partId', function (req, res) {
 
     var query = "SELECT * FROM inventory WHERE id = " + req.params.partId,
-      connection = mysql.createConnection(mysqlConnection);
+    connection = mysql.createConnection(mysqlConnection);
 
     connection.connect();
 
@@ -55,9 +56,9 @@
   app.get(['/catalog/:search','/catalog/'], function (req, res) {
 
     var query = '',
-      connection = mysql.createConnection(mysqlConnection),
-      search = '',
-      items = [];
+    connection = mysql.createConnection(mysqlConnection),
+    search = '',
+    items = [];
 
     //prepare sql query
     if (req.params.search) {
@@ -72,15 +73,49 @@
     query = connection.query(query);
 
     query
-      .on('result', function (row, index) {
-          items[items.length] = row;
-      })
-      .on('end', function () {
-        res.render('search', {searchPhrase: req.params.search, items: items, length: items.length});
-      });
+    .on('result', function (row, index) {
+      items[items.length] = row;
+    })
+    .on('end', function () {
+      res.render('search', {searchPhrase: req.params.search, items: items, length: items.length});
+    });
 
     connection.end();
 
+  });
+
+
+
+  app.post('/api/sendmail/:phone', function (req, res) {
+    // create reusable transporter object using the default SMTP transport
+    var transporter = nodemailer.createTransport({
+      host: 'smtp.mail.ru',
+      port: 587,
+      secure: false, // secure:true for port 465, secure:false for port 587,
+      auth: {
+        user: 'sales2@seltex.ru',
+        pass: '1qwerty12345'
+      }
+    }),
+
+    // setup email data with unicode symbols
+    mailOptions = {
+      from: '"SELTEX.RU" <sales2@seltex.ru>', // sender address
+      to: 'sales@seltex.com', // list of receivers
+      subject: 'Заказ звонка с сайта', // Subject line
+      text: 'Перезвонить на номер: ' + req.params.phone, // plain text body
+      html: '<h3>Перезвонить на номер: ' + req.params.phone + '</h3>' // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        // return console.log(error);
+        res.send(false);
+      }
+      res.send('OK');
+      // console.log('Message %s sent: %s', info.messageId, info.response);
+    });
   });
 
 
