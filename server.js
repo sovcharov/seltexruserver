691154ca5f25d7b0c524d0c75d7bb676d79fd2fd
yahooserg <seltexinc@gmail.com>
2018-08-24 14:42:19 +0300

    test

--- server.js
+++ server.js
@@ -94,7 +94,7 @@
         }
         // console.log(req.params.partId);
         // console.log(rows);
-        if(rows[0]allNumbers[0]!==""){
+        if(rows[0].allNumbers[0]!==""){
           query = "SELECT p.description, p.comment, p.weight, inventoryManufacturers.fullName as manufacturerFullName, inventoryNumbers.number, p.id, p.price, p.stock, p.ordered, p.link from inventoryNumbers, inventory as p, inventoryManufacturers where inventoryManufacturers.id = inventoryNumbers.manufacturerId and inventoryNumbers.inventoryId = p.id and p.id <> " + req.params.partId + " and inventoryNumbers.number = '" + rows[0].allNumbers[0].number + "' order by inventoryNumbers.main desc";
           // console.log(query);
           var part = rows[0];
