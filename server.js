050c7ef8f3b75a739b3791c620760d5de6c2b31b
yahooserg <seltexinc@gmail.com>
2018-08-24 14:11:09 +0300

    test

--- server.js
+++ server.js
@@ -94,7 +94,7 @@
         }
         // console.log(req.params.partId);
         // console.log(rows);
-        query = "SELECT p.description, p.comment, p.weight, inventoryManufacturers.fullName as manufacturerFullName, inventoryNumbers.number, p.id, p.price, p.stock, p.ordered, p.link from inventoryNumbers, inventory as p, inventoryManufacturers where inventoryManufacturers.id = inventoryNumbers.manufacturerId and inventoryNumbers.inventoryId = p.id and p.id <> " + req.params.partId + " and inventoryNumbers.number = " + row[0].allNumbers[0].number + " order by inventoryNumbers.main desc";
+        query = "SELECT p.description, p.comment, p.weight, inventoryManufacturers.fullName as manufacturerFullName, inventoryNumbers.number, p.id, p.price, p.stock, p.ordered, p.link from inventoryNumbers, inventory as p, inventoryManufacturers where inventoryManufacturers.id = inventoryNumbers.manufacturerId and inventoryNumbers.inventoryId = p.id and p.id <> " + req.params.partId + " and inventoryNumbers.number = " + rows[0].allNumbers[0].number + " order by inventoryNumbers.main desc";
 
         connection.query(query, function (err, results, fields) {
           rows[0].analogs = results;
